b4b1b5dfe727a56a4f28d326c18aac47c8f0bf09
hoani <hoani.bryson@gmail.com>
2020-02-25 09:28:21 +1300

    Add contract decoding functions and tests

--- src/cennznut/v0/contract.rs
+++ src/cennznut/v0/contract.rs
@@ -13,14 +13,8 @@ extern crate alloc;
 #[cfg(feature = "std")]
 extern crate std as alloc;
 
-// use alloc::borrow::ToOwned;
-// use alloc::string::{String, ToString};
-// use alloc::vec::Vec;
 use bit_reverse::ParallelReverse;
-use codec::{Encode, Output};
-// use codec::{Decode, Encode, Input, Output};
-
-// use super::WILDCARD;
+use codec::{Decode, Encode, Input, Output};
 
 /// A CENNZnet permission domain contract
 #[cfg_attr(test, derive(Clone, Debug, Eq, PartialEq))]
@@ -68,53 +62,45 @@ impl Encode for Contract {
     }
 }
 
-// impl Decode for Contract {
-//     fn decode<I: Input>(input: &mut I) -> Result<Self, codec::Error> {
-//         let block_cooldown_and_method_count: u8 = input.read_byte()?.swap_bits();
-//         let method_count = block_cooldown_and_method_count >> 1;
+impl Decode for Contract {
+    fn decode<I: Input>(input: &mut I) -> Result<Self, codec::Error> {
+        let has_cooldown_byte: u8 = input.read_byte()?.swap_bits();
+        let has_cooldown: bool = (has_cooldown_byte & 0x01) == 0x01;
 
-//         let mut name_buf: [u8; 32] = Default::default();
-//         input
-//             .read(&mut name_buf)
-//             .map_err(|_| "expected 32 byte module name")?;
-//         let name = core::str::from_utf8(&name_buf)
-//             .map_err(|_| codec::Error::from("module names should be utf8 encoded"))?
-//             .trim_matches(char::from(0))
-//             .to_string();
+        let mut address: [u8; 32] = Default::default();
+        input
+            .read(&mut address)
+            .map_err(|_| "expected 32 byte address")?;
 
-//         let module_cooldown =
-//             if (block_cooldown_and_method_count.swap_bits() & 0b1000_0000) == 0b1000_0000 {
-//                 Some(u32::from_le_bytes([
-//                     input.read_byte()?.swap_bits(),
-//                     input.read_byte()?.swap_bits(),
-//                     input.read_byte()?.swap_bits(),
-//                     input.read_byte()?.swap_bits(),
-//                 ]))
-//             } else {
-//                 None
-//             };
+        let block_cooldown = if has_cooldown {
+            Some(u32::from_le_bytes([
+                input.read_byte()?.swap_bits(),
+                input.read_byte()?.swap_bits(),
+                input.read_byte()?.swap_bits(),
+                input.read_byte()?.swap_bits(),
+            ]))
+        } else {
+            None
+        };
 
-//         let mut methods: Vec<(String, Method)> = Vec::default();
+        if Some(0) != input.remaining_len().unwrap() {
+            return Err(codec::Error::from("payload too long"));
+        }
 
-//         for _ in 0..method_count {
-//             let m: Method = Decode::decode(input)?;
-//             methods.push((m.name.to_owned(), m));
-//         }
-
-//         Ok(Self {
-//             name,
-//             block_cooldown: module_cooldown,
-//             methods,
-//         })
-//     }
-// }
+        Ok(Self {
+            address,
+            block_cooldown,
+        })
+    }
+}
 
 #[cfg(test)]
 mod test {
     use super::Contract;
-    use codec::{Encode, Output};
+    use codec::{Decode, Encode};
     use std::assert_eq;
 
+    // Constructor tests
     #[test]
     fn it_initializes() {
         let expected: usize = 32;
@@ -172,6 +158,7 @@ mod test {
         assert_eq!(contract.block_cooldown, Option::Some(0x1337_b33f));
     }
 
+    // Encoding Tests
     #[test]
     fn it_encodes_basic_case() {
         let contract = Contract::new(&[
@@ -230,5 +217,84 @@ mod test {
         assert_eq!(result[address_length + 2], 0xcd);
         assert_eq!(result[address_length + 3], 0xec);
         assert_eq!(result[address_length + 4], 0xc8);
+    }
+
+    // Decoding tests
+    #[test]
+    fn it_decodes_basic_case() {
+        let contract_expected = Contract::new(&[
+            0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16,
+            0x17, 0x18, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27, 0x28, 0x31, 0x32, 0x33, 0x34,
+            0x35, 0x36, 0x37, 0x38,
+        ]);
+
+        let encoded = vec![
+            0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x11, 0x12, 0x13, 0x14, 0x15,
+            0x16, 0x17, 0x18, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27, 0x28, 0x31, 0x32, 0x33,
+            0x34, 0x35, 0x36, 0x37, 0x38,
+        ];
+
+        let constract_result = Contract::decode(&mut &encoded[..]).expect("it works");
+
+        assert_eq!(constract_result.encode(), encoded);
+        assert_eq!(constract_result, contract_expected);
+    }
+
+    #[test]
+    fn it_decodes_block_cooldown() {
+        let contract_expected = Contract::wildcard().block_cooldown(0x1337_b33f);
+
+        // 0b1000_0000 = cooldown flag (note: bits get flipped)
+        let encoded = vec![
+            0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0xfc, 0xcd, 0xec, 0xc8,
+        ];
+
+        let constract_result = Contract::decode(&mut &encoded[..]).expect("it works");
+
+        assert_eq!(constract_result.encode(), encoded);
+        assert_eq!(constract_result, contract_expected);
+    }
+
+    #[test]
+    fn it_throws_error_on_short_address_length() {
+        let encoded = vec![
+            0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        ];
+
+        assert_eq!(
+            Contract::decode(&mut &encoded[..]),
+            Err(codec::Error::from("expected 32 byte address")),
+        );
+    }
+
+    #[test]
+    fn it_throws_error_on_unannounced_bytes() {
+        let encoded = vec![
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0xfc, 0xcd, 0xec, 0xc8,
+        ];
+
+        assert_eq!(
+            Contract::decode(&mut &encoded[..]),
+            Err(codec::Error::from("payload too long")),
+        );
+    }
+
+    #[test]
+    fn it_throws_error_on_missing_cooldown() {
+        let encoded = vec![
+            0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00,
+        ];
+
+        assert_eq!(
+            Contract::decode(&mut &encoded[..]),
+            Err(codec::Error::from("Not enough data to fill buffer")),
+        );
     }
 }
