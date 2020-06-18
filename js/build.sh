357073a93b23d5a390cc85097beb7c0740253fcd
Jordan <beauchjord@gmail.com>
2020-06-18 16:14:30 +1200

    Fix rogue file exclusion during 'npm pack' (#58)
    * Fix rogue file exclusion during 'npm pack'
    Remove un-needed files from npm package
    * Add LICENSE, update .npmignore
    Update READEME
    
    * Update package.json: files

--- js/build.sh
+++ js/build.sh
@@ -1,8 +1,13 @@
 # compile the rust codebase
 echo "building js pkg for $1 out to: $2"
- wasm-pack build \
-     --target $1  \
-     --scope cennznet \
-     --out-name cennznut \
-     --out-dir $2 \
-     --release
+wasm-pack build \
+    --target $1 \
+    --scope cennznet \
+    --out-name cennznut \
+    --out-dir $2 \
+    --release
+
+# Remove wasm-pack generated files
+# They are unintentionally excluding required files when `npm pack` is run
+rm -rf $2/{package.json,README.md,.gitignore,LICENSE}
+
